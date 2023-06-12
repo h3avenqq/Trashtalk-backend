8d4cc0fb8a2a070dd44f8c64dfde6173ba870b7e
Maxim Shamrev <shmarev082@gmail.com>
2023-06-12 20:18:15 +0300

    added application layer with mapper and DI (mediatr) + db context

---
+++ Trashtalk.Backend/Trashtalk.Application/Common/Mappings/AssemblyMapingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Trashtalk.Application.Common.Mappings
+{
+    public class AssemblyMapingProfile : Profile
+    {
+        public AssemblyMapingProfile(Assembly assembly)
+        {
+            ApplyMappingsAssembly(assembly);
+        }
+
+        private void ApplyMappingsAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type=>type.GetInterfaces()
+                    .Any(i=>i.IsGenericType &&
+                    i.GetGenericTypeDefinition()==typeof(IMapWith<>)))
+                .ToList();
+
+            foreach(var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var method = type.GetMethod("Mapping");
+                method?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ Trashtalk.Backend/Trashtalk.Application/Interfaces/ITrashTalkDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using Trashtalk.Domain;
+
+namespace Trashtalk.Application.Interfaces
+{
+    public interface ITrashTalkDbContext
+    {
+        DbSet<News> News { get; set; }
+        DbSet<ReceptionPoint> ReceptionPoints { get; set; }
+        DbSet<Trash> Trash { get; set; }
+        DbSet<TrashType> TrashTypes { get; set; }
+        DbSet<TrashBin> TrashBins { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
