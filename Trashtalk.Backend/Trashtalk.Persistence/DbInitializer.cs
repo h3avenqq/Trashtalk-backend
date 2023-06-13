061fe3485b54e73d413603535a5d86c08644aab6
Maxim Shamrev <shmarev082@gmail.com>
2023-06-14 00:08:30 +0300

    added initialize method for db

---
+++ Trashtalk.Backend/Trashtalk.Persistence/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Trashtalk.Persistence
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TrashTalkDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
