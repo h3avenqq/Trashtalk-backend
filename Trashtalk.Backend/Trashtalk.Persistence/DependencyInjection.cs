537f36eeb5af1c05ec8dd029a0651910c830fd88
Maxim Shamrev <shmarev082@gmail.com>
2023-06-24 02:00:04 +0300

    added di for persistence

---
+++ Trashtalk.Backend/Trashtalk.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Trashtalk.Application.Interfaces;
+
+namespace Trashtalk.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connection = configuration["DbConnection"];
+            services.AddDbContext<TrashTalkDbContext>(opt =>
+            {
+                opt.UseNpgsql(connection);
+            });
+
+            services.AddScoped<ITrashTalkDbContext>(provider =>
+                provider.GetService<TrashTalkDbContext>());
+
+            return services;
+        }
+    }
+}
