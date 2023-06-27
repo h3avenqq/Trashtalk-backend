fdef69cf7930a3411f6080897aa9b49aedba4fbd
Maxim Shamrev <shmarev082@gmail.com>
2023-06-27 03:20:22 +0300

    api for news

---
+++ Trashtalk.Backend/Trashtalk.WebAPI/Controllers/NewsController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Trashtalk.Application.CQRS.News.Commands.CreateNews;
+using Trashtalk.Application.CQRS.News.Commands.DeleteNews;
+using Trashtalk.Application.CQRS.News.Queries.GetNewsDetails;
+using Trashtalk.Application.CQRS.News.Queries.GetNewsList;
+using Trashtalk.WebAPI.Models;
+
+namespace Trashtalk.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class NewsController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public NewsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<NewsListVm>> GetAll()
+        {
+            var query = new GetNewsListQuery();
+
+            var vm = await Mediatr.Send(query);
+
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NewsDetailsVm>> Get(Guid id)
+        {
+            var query = new GetNewsDetailsQuery()
+            {
+                Id = id
+            };
+
+            var vm = await Mediatr.Send(query);
+
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateNewsDto createNewsDto)
+        {
+            var command = _mapper.Map<CreateNewsCommand>(createNewsDto);
+
+            var newsId = await Mediatr.Send(command);
+
+            return Ok(newsId);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update([FromBody] UpdateNewsDto updateNewsDto)
+        {
+            var command = _mapper.Map<UpdateNewsDto>(updateNewsDto);
+
+            await Mediatr.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var command = new DeleteNewsCommand() 
+            { 
+                Id = id 
+            };
+
+            await Mediatr.Send(command);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Trashtalk.Backend/Trashtalk.WebAPI/Models/CreateNewsDto.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using Trashtalk.Application.Common.Mappings;
+using Trashtalk.Application.CQRS.News.Commands.CreateNews;
+
+namespace Trashtalk.WebAPI.Models
+{
+    public class CreateNewsDto : IMapWith<Domain.News>
+    {
+        public string Title { get; set; }
+        public string BriefDescription { get; set; }
+        public string Description { get; set; }
+        public Guid AuthorId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateNewsDto, CreateNewsCommand>()
+                .ForMember(x => x.Title, opt => opt.MapFrom(x => x.Title))
+                .ForMember(x => x.BriefDescription, opt => opt.MapFrom(x => x.BriefDescription))
+                .ForMember(x => x.Description, opt => opt.MapFrom(x => x.Description))
+                .ForMember(x => x.AuthorId, opt => opt.MapFrom(x => x.AuthorId));
+        }
+    }
+}
