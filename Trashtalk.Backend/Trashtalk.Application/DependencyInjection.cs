f8fe633e78dfc9ddf887b522f2c32cd6b1bc5737
Maxim Shamrev <shmarev082@gmail.com>
2023-07-03 02:06:11 +0300

    add di for validation and pipline behavior for it

--- Trashtalk.Backend/Trashtalk.Application/DependencyInjection.cs
+++ Trashtalk.Backend/Trashtalk.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using Trashtalk.Application.Common.Behaviors;
 
 namespace Trashtalk.Application
 {
@@ -8,7 +11,8 @@ namespace Trashtalk.Application
         public static IServiceCollection AddApplication(this  IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
+            services.AddValidatorsFromAssemblies(new[] {Assembly.GetExecutingAssembly()});
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             return services;
         }
     }
