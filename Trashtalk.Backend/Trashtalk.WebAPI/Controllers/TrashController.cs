765ed6b135b0451cf569fead042c4b63567b5822
Maxim Shamrev <shmarev082@gmail.com>
2023-07-04 16:34:55 +0300

    remove unnecessary usings

--- Trashtalk.Backend/Trashtalk.WebAPI/Controllers/TrashController.cs
+++ Trashtalk.Backend/Trashtalk.WebAPI/Controllers/TrashController.cs
@@ -2,9 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Trashtalk.Application.CQRS.Trash.Commands.CreateTrash;
 using Trashtalk.Application.CQRS.Trash.Commands.DeleteTrash;
